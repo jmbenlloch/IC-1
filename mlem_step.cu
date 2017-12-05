3b30348a8d610ab9b502092f0009358e1f15d4ed
Jose Maria Benlloch Rodriguez <jmbenlloch@gpu1next.uv.es>
2017-12-05 23:22:56 +0100

    Efficiency checked

--- mlem_step.cu
+++ mlem_step.cu
@@ -18,15 +18,20 @@ __global__ void mlem_step(voxel * voxels, voxel * voxels_out,
 		bool * selection,
 		int nvoxels, int nsipms, int npmts){
 
+	//printf("[%d], x: %f, y: %f, e: %f\n", blockIdx.x, voxels[blockIdx.x].x, voxels[blockIdx.x].y, voxels[blockIdx.x].E);
+
 	float efficiency = 0;
 
 	// Add SiPM part (this can be stored on the first iteration)
+	//Efficiency for sipms seems to be ok compared with python version (check dtype of sipm_prob...)
 	for(int i=0; i<nsipms; i++){
+	//	printf("sipm_prob[%d]: %f\n", blockIdx.x * nsipms + i, sipm_prob[blockIdx.x * nsipms + i]);
 		if(selection[blockIdx.x * nsipms + i]){
 			efficiency += sipm_prob[blockIdx.x * nsipms + i];
+			//printf("eff: %f, new val: %f\n", efficiency, sipm_prob[blockIdx.x * nsipms + i]);
 		}
 	}
-	//TODO do the same for cathode
+
 
 	// Forward projection anode
 	float anode_forward = 0;
@@ -40,15 +45,23 @@ __global__ void mlem_step(voxel * voxels, voxel * voxels_out,
 				}
 			}
 			anode_forward += num/denom;
+			if(isnan(denom)){
+			//	printf("[%d], num: %f, den: %f, forward: %f\n", blockIdx.x, num, denom, anode_forward);
+			}
 		}
 	}
 
 	float result = voxels[blockIdx.x].E / efficiency * anode_forward;
-	printf("voxel %d, eff: %f, forward: %f, energy: %f\n", blockIdx.x, efficiency, anode_forward, result);
+	//printf("voxel %d, eff: %f, forward: %f, energy: %f\n", blockIdx.x, efficiency, anode_forward, result);
 
 	voxel * v = voxels_out + blockIdx.x;
 	v->x = voxels[blockIdx.x].x;
 	v->y = voxels[blockIdx.x].y;
-	v->E = result;
+	if (voxels[blockIdx.x].E <= 0.){
+		//printf("voxel %d, eff: %f, forward: %f, energy: %f\n", blockIdx.x, efficiency, anode_forward, result);
+		v->E = 0.;
+	}else{
+		v->E = result;
+	}
 }
 
