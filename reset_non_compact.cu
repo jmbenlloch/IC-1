f8806a5f243b23c587776100f5eae024475a72ef
Jose Maria Benlloch Rodriguez <jmbenlloch@gpu1next.uv.es>
2018-01-15 18:22:12 +0100

    Almost working version

--- reset_non_compact.cu
+++ reset_non_compact.cu
@@ -69,10 +69,11 @@ __global__ void create_anode_response(sensor * sensors,
 }
 
 // Launch < nvoxels >
-// active[nvoxels][nsensors]
-__global__ void compute_active_sensors(bool * active, sensor * sensors,
-		voxel * voxels, float * xs, float * ys,
-		int nsensors, float sipm_dist){
+// active[nvoxels][nsensors]  / probabilities[nvoxels][nsensors]
+__global__ void compute_active_sensors(bool * active, float * probabilities,
+		sensor * sensors, voxel * voxels, float * xs, float * ys,
+		int nsensors, float sensor_dist,
+		float step, int nbins, float xmin, float ymin, correction * corrections){
 	for(int i=0; i<nsensors; i++){
 		int idx = blockIdx.x * nsensors + i;
 		int id  = sensors[i].id;
@@ -80,9 +81,80 @@ __global__ void compute_active_sensors(bool * active, sensor * sensors,
 		float xdist = voxels[blockIdx.x].x - xs[id];
 		float ydist = voxels[blockIdx.x].y - ys[id];
 
-		active[idx] = ((abs(xdist) <= sipm_dist) && 
-				       (abs(ydist) <= sipm_dist));
+		bool voxel_sensor = ((abs(xdist) <= sensor_dist) && 
+		                     (abs(ydist) <= sensor_dist));
+		active[idx] = voxel_sensor;
 
-//		printf("[%d]: id=%d, xdist=%f, ydist=%f, active=%d\n", blockIdx.x, id, xdist, ydist, active[idx]);
+		// Compute probability
+		// In order to avoid accesing wrong parts of the memory 
+		// if the sensor is not active for a particular voxel,
+		// then we will use index 0.
+		// Rounding: plus 0.5 and round down
+		int xindex = __float2int_rd((xdist - xmin) / step * voxel_sensor + 0.5f);
+		int yindex = __float2int_rd((ydist - ymin) / step * voxel_sensor + 0.5f);
+		int prob_idx = xindex * nbins + yindex;
+		probabilities[idx] = corrections[prob_idx].factor;
+//		printf("[%d]: idx=%d, p=%f, pidx=%d, xindex=%d, %f, nbins=%d, yindex=%d, %f\n", blockIdx.x, idx, probabilities[idx], prob_idx, xindex, xdist, nbins, yindex, ydist);
+
+//		printf("[%d]: id=%d, xdist=%f, ydist=%f, active=%d\n", blockIdx.x, id, xdist, ydist, active[idx], probabilities[idx]);
+
 	}
 }
+
+__global__ void mlem_step(voxel * voxels, voxel * voxels_out,
+		sensor * anode_response, float * cath_response,
+		float * pmt_prob, float * sipm_prob,
+		bool * sipm_selection, bool * pmt_selection,
+		int nvoxels, int nsipms, int npmts){
+
+	printf("[%d], active: %d, x: %f, y: %f, e: %f\n", blockIdx.x, voxels[blockIdx.x].active, voxels[blockIdx.x].x, voxels[blockIdx.x].y, voxels[blockIdx.x].E);
+
+	float efficiency = 0;
+
+	// Add SiPM part (this can be stored on the first iteration)
+	for(int i=0; i<nsipms; i++){
+		if(sipm_selection[blockIdx.x * nsipms + i]){
+			efficiency += sipm_prob[blockIdx.x * nsipms + i];
+		}
+	}
+	// Add cathode part (just one pmt now)
+	for(int i=0; i<npmts; i++){
+		if(pmt_selection[blockIdx.x * npmts + i]){
+			efficiency += pmt_prob[blockIdx.x * npmts + i];
+		}
+	}
+
+	// Forward projection anode
+	float anode_forward = 0;
+	for(int i=0; i<nsipms; i++){
+		if(sipm_selection[blockIdx.x * nsipms + i]){
+			float num = anode_response[i].charge * sipm_prob[blockIdx.x * nsipms + i];
+			float denom = 0;
+			for(int j=0; j<nvoxels; j++){
+				if(sipm_selection[j * nsipms + i]){
+					denom += voxels[j].E * sipm_prob[j * nsipms + i];
+				}
+			}
+			anode_forward += num/denom;
+		}
+	}
+
+	// Forward projection cathode
+	float num = cath_response[0] * pmt_prob[blockIdx.x];
+	float denom = 0;
+	for(int j=0; j<nvoxels; j++){
+		denom += voxels[j].E * pmt_prob[j];
+	}
+	float cathode_forward = num / denom;
+	float result = voxels[blockIdx.x].E / efficiency * (anode_forward + cathode_forward);
+
+	voxel * v = voxels_out + blockIdx.x;
+	v->x = voxels[blockIdx.x].x;
+	v->y = voxels[blockIdx.x].y;
+	if (voxels[blockIdx.x].E <= 0.){
+		v->E = 0.;
+	}else{
+		v->E = result;
+	}
+}
+
