4cb76c3161ad5b2bd13d6cd1d4df0b1685d3cb87
Jose Maria Benlloch Rodriguez <jmbenlloch@gpu1next.uv.es>
2018-05-17 12:28:29 +0200

    Run RESET over all S2s that passed filters

--- invisible_cities/cities/reset.py
+++ invisible_cities/cities/reset.py
@@ -27,39 +27,44 @@ class Reset(ResetCity):
         s2_index = np.argmax(filter_output.s2_peaks)
 
         s1 = pmapVectors.pmaps.s1s[s1_index]
-        s2 = pmapVectors.pmaps.s2s[s2_index]
 
         ZCorr = None
         if self.conf.lifetime_corr:
             ZCorr = corrf.LifetimeCorrection(self.conf.lifetime_value,
                                              self.conf.lifetime_error)
 
-        reset_data   = rst_utils.prepare_data(s1, s2, self.conf.rebin_factor, self.DataSiPM,
+        voxels_out = []
+
+        # RESET has to be run for all the S2 in the event
+        for s2_index in np.where(filter_output.s2_peaks)[0]:
+            s2 = pmapVectors.pmaps.s2s[s2_index]
+
+            reset_data   = rst_utils.prepare_data(s1, s2, self.conf.rebin_factor, self.DataSiPM,
                                               self.conf.nsipms, self.conf.sipm_thr,
                                               self.conf.dist, ZCorr)
 
-        # Check some data has passed all the filters
-        if reset_data.slices.nslices == 0:
-            return []
+            # Check some data has passed all the filters
+            if reset_data.slices.nslices == 0:
+                continue
 
-        slices_start = rst_utils.slices_start(reset_data.voxels_data,
-                                              self.conf.x_size,
-                                              self.conf.y_size)
+            slices_start = rst_utils.slices_start(reset_data.voxels_data,
+                                                  self.conf.x_size,
+                                                  self.conf.y_size)
 
-        voxels, slices = self.rst.run(reset_data.voxels_data, reset_data.slices,
-                                 reset_data.energies, slices_start,
-                                 self.conf.iterations)
+            voxels, slices = self.rst.run(reset_data.voxels_data, reset_data.slices,
+                                     reset_data.energies, slices_start,
+                                     self.conf.iterations)
 
-        voxels_out = []
-        for i, v in enumerate(voxels):
-            voxel = {}
-            voxel['event'] = pmapVectors.events
-            voxel['x']     = v[0]
-            voxel['y']     = v[1]
-            voxel['z']     = reset_data.zs[slices[i]]
-            voxel['E']     = v[2]
-
-            voxels_out.append(voxel)
+            for i, v in enumerate(voxels):
+                # Filter voxels with zero energy
+                if v[2] > 0:
+                    voxel = {}
+                    voxel['event'] = pmapVectors.events
+                    voxel['x']     = v[0]
+                    voxel['y']     = v[1]
+                    voxel['z']     = reset_data.zs[slices[i]]
+                    voxel['E']     = v[2]
+                    voxels_out.append(voxel)
 
         return voxels_out
 
