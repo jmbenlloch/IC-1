c0a2f520ec6115f50eaf2eabbe771fab9b6e4479
Jose Maria Benlloch Rodriguez <jmbenlloch@gpu1next.uv.es>
2018-05-28 11:47:21 +0200

    Add utility functions to correct lifetime and create reset dst

--- invisible_cities/io/voxels_io.py
+++ invisible_cities/io/voxels_io.py
@@ -1,5 +1,6 @@
-from invisible_cities.evm.nh5           import VoxelsTable
-from .  table_io           import make_table
+from invisible_cities.evm.nh5 import VoxelsTable
+from invisible_cities.evm.nh5 import ResetDstTable
+from .  table_io              import make_table
 
 # writers
 def voxels_writer(hdf5_file, *, compression='ZLIB4'):
@@ -44,3 +45,28 @@ def load_voxels(DST_file_name):
         voxel = Voxel(X[i], Y[i], Z[i], E[i], size[i])
         current_event.voxels.append(voxel)
     return all_events
+
+
+def dst_writer(hdf5_file, *, compression='ZLIB4'):
+    dst_table  = make_table(hdf5_file,
+                            group       = 'ResetDst',
+                            name        = 'Events',
+                            fformat     = ResetDstTable,
+                            description = 'RESET DST',
+                            compression = compression)
+
+    def write_dst(df):
+        for df_row in df.iterrows():
+            row = dst_table.row
+            row["evt"  ] = df_row[0]
+            row["Xmin" ] = df_row[1][0]
+            row["Ymin" ] = df_row[1][1]
+            row["Zmin" ] = df_row[1][2]
+            row["Xmax" ] = df_row[1][3]
+            row["Ymax" ] = df_row[1][4]
+            row["Zmax" ] = df_row[1][5]
+            row["E"    ] = df_row[1][6]
+            row["Ecorr"] = df_row[1][7]
+            row.append()
+
+    return write_dst
